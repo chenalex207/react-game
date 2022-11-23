a102596995ee518f84055437478d1f90d9a3dedf
chenalex207 <goodytube77@gmail.com>
2022-11-23 18:17:57 +0800

    Updates

---
+++ precache-manifest.0d885f99408c025582a2e36308e38bec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ece7a06876105547e500",
+    "url": "/react-game/static/css/main.961f913e.chunk.css"
+  },
+  {
+    "revision": "ece7a06876105547e500",
+    "url": "/react-game/static/js/main.ece7a068.chunk.js"
+  },
+  {
+    "revision": "2a72130e3f2bac027304",
+    "url": "/react-game/static/js/1.2a72130e.chunk.js"
+  },
+  {
+    "revision": "cda9c7c5162c773fb401",
+    "url": "/react-game/static/js/runtime~main.cda9c7c5.js"
+  },
+  {
+    "revision": "f95b88cb841c001cc55215905e09c18b",
+    "url": "/react-game/index.html"
+  }
+];
